829211e9dc42096a77fa8e9dde668fdfa48dfad0
Nagybhe Hage <65202520+nagybhe@users.noreply.github.com>
2021-02-03 12:21:10 -0300

    PHP

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta name="author" content="nagybhe">
+    <meta name="description" content="Crud">
+    <meta name="keywords" content="#">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Visão Geral</title>
+    
+</head>
+
+<body>
+        <div class="container">
+          <div class="jumbotron">
+            <div class="row">
+                <h2><span class="badge badge-secondary">SIS-CURE cadastro de pessoas v1.0.0 </span></h2>
+            </div>
+          </div>
+            </br>
+            <div class="row">
+                <p>
+                    <a href="create.php" class="btn btn-success">Cadastrar</a>
+                </p>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Sexo</th>
+                            <th scope="col">Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM pessoa ORDER BY id DESC';
+
+                        foreach($pdo->query($sql)as $row)
+                        {
+                            echo '<tr>';
+			                      echo '<th scope="row">'. $row['id'] . '</th>';
+                            echo '<td>'. $row['nome'] . '</td>';
+                            echo '<td>'. $row['endereco'] . '</td>';
+                            echo '<td>'. $row['telefone'] . '</td>';
+                            echo '<td>'. $row['email'] . '</td>';
+                            echo '<td>'. $row['sexo'] . '</td>';
+                            echo '<td width=250>';
+                            echo '<a class="btn btn-primary" href="read.php?id='.$row['id'].'">Info</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-warning" href="update.php?id='.$row['id'].'">Atualizar</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Excluir</a>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                        Banco::desconectar();
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
